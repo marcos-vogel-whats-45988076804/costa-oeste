2f82c4cc908e95dc68720864d8c095ad946e2dc0
marcos-vogel-whats-45988076804 <eletronicospy@outlook.com>
2026-02-11 22:57:45 -0300

    Update contador.php

--- contador.php
+++ contador.php
@@ -1,16 +1,21 @@
 <?php
 $file = 'contador.json';
 
+// Se não existir, cria com 0
 if (!file_exists($file)) {
     file_put_contents($file, json_encode(["visitas" => 0]));
 }
 
+// Lê o JSON
 $data = json_decode(file_get_contents($file), true);
 
+// Incrementa
 $data['visitas']++;
 
+// Salva de volta
 file_put_contents($file, json_encode($data));
 
+// Retorna o JSON atualizado
 header('Content-Type: application/json');
 echo json_encode($data);
 ?>
